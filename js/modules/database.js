9c7d18179fa9990981a4cb6a829fc40393cd7ec8
kazimov789 <118839541+kazimov789@users.noreply.github.com>
2023-03-22 04:50:47 +0400

    HR-System Ready

---
+++ js/modules/database.js
@@ -0,0 +1,19 @@
+const dataBase = [
+  {
+    id: 1,
+    name: "Rahid",
+    surname: "Kazimov",
+    salary: 1800,
+    duty: "Developer",
+  },
+  {
+    id: 2,
+    name: "Muhammed",
+    surname: "Mikayilov",
+    salary: 8400,
+    duty: "Developer",
+  },
+];
+const loginDatas = [];
+
+export { dataBase,loginDatas };
---
+++ js/modules/dom-selectors.js
@@ -0,0 +1,50 @@
+//pages
+const mainArea = document.querySelector("#main-page");
+const loginArea = document.querySelector("#login-page");
+const addArea = document.querySelector("#add-page");
+
+const loginBtn = document.querySelector("#login");
+const navLinks = document.querySelectorAll(".nav-link");
+const deletePageTable = document.querySelector("#delete-page table");
+
+const employeeTable = document.querySelector("#main-page table");
+const deletePage = document.querySelector("#delete-page");
+let addEmployee = document.querySelectorAll("#add-page input");
+const AddBtn = document.querySelector("#employeeAddBtn");
+const sortName = document.querySelector("#sortName");
+const sortSurName = document.querySelector("#sortSurName");
+const sortSalary = document.querySelector("#sortSalary");
+const sortDuty = document.querySelector("#sortDuty");
+const showSalaryBtn = document.querySelector("#salaryFilterBTN");
+const searchBtn = document.querySelector("#searchBtn");
+const employeeTableHeadCard = `
+<thead>
+<tr>
+  <th scope="col">#</th>
+  <th scope="col">Ad</th>
+  <th scope="col">Soyad</th>
+  <th scope="col">Maaş</th>
+  <th scope="col">Vəzifə</th>
+</tr>
+</thead>
+<tr>`;
+// employeeTable,deletePage,addEmployee,AddBtn,sortName,sortSurName,sortSalary,sortDuty,showSalaryBtn,searchBtn
+export {
+  mainArea,
+  loginArea,
+  addArea,
+  loginBtn,
+  navLinks,
+  deletePageTable,
+  employeeTable,
+  deletePage,
+  addEmployee,
+  AddBtn,
+  sortName,
+  sortSurName,
+  sortSalary,
+  sortDuty,
+  showSalaryBtn,
+  searchBtn,
+  employeeTableHeadCard,
+};
