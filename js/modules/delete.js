155bf8fd9ff1935664ce7ffc7813fbf02367cabb
kazimov789 <118839541+kazimov789@users.noreply.github.com>
2023-03-28 13:20:15 +0400

    added modal

--- js/modules/delete.js
+++ js/modules/delete.js
@@ -1,13 +1,25 @@
 import { dataShowss } from "./dataShows.js";
 import { dataBase } from "./database.js";
-import { deletePageTable,employeeTableHeadCard } from "./dom-selectors.js";
+import { deletePageTable, employeeTableHeadCard,deletePage } from "./dom-selectors.js";
 
 export function deleteFeature() {
-  document.querySelector("#delete-page button").addEventListener("click", () => {
+  document
+    .querySelector("#delete-page button")
+    .addEventListener("click", () => {
+      const deleteModal = document.querySelector("#deleteModal");
       const deletePersonID = document.querySelector("#delete-page input").value;
-      // dataBase.splice(deletePersonID-1,1);
-      dataBase[deletePersonID - 1] = "";
-      alert(`#${deletePersonID}-IDli insan sistemden silindi`);
-      dataShowss(deletePageTable,dataBase,employeeTableHeadCard);
+      deletePageTable.classList.add("d-none");
+      deleteModal.classList.remove("d-none");
+      document.querySelector("#modalCloseBtn").addEventListener("click", () => {
+        deleteModal.classList.add("d-none");
+        deletePageTable.classList.remove("d-none");
+      });
+      document.querySelector("#modalSaveBtn").addEventListener("click", () => {
+        dataBase[deletePersonID - 1] = "";
+        alert(`#${deletePersonID}-IDli insan sistemden silindi`);
+        dataShowss(deletePageTable, dataBase, employeeTableHeadCard);
+        deleteModal.classList.add("d-none");
+        deletePageTable.classList.remove("d-none");
+      });
     });
 }
